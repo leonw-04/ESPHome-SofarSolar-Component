6c9fabe247f2eac2a1a13ae5a71f89d00c72b47b
Leon Westhoff <leon.westhoff@pannek.eu>
2025-07-07 04:02:03 +0200

    refactor: simplify response validation logic in sofarsolar_inverter

--- esphome/components/sofarsolar_inverter/sofarsolar_inverter.cpp
+++ esphome/components/sofarsolar_inverter/sofarsolar_inverter.cpp
@@ -58,7 +58,7 @@ namespace esphome {
                     ESP_LOGE(TAG, "No response received");
                 } else {
                     current_reading = false;
-                    if (check_crc(response) && response[0] == 0x01 && response[1] == 0x03 && response[2] == 0x04) {
+                    if (check_crc(response) && response[0] == 0x01 && response[1] == 0x03) {
                     // Process the response based on the register type
                         uint16_t start_address = registers_G3[register_tasks.top().register_index].start_address;
                         uint8_t quantity = registers_G3[register_tasks.top().register_index].quantity;
