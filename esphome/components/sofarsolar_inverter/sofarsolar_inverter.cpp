9581bb7a59b74ae5d6df42263429077c76b971b1
Leon Westhoff <lwestho2@uni-muenster.de>
2026-02-11 22:46:28 +0100

    fix: update zero export status and set power control parameters in SofarSolar Inverter

--- esphome/components/sofarsolar_inverter/sofarsolar_inverter.cpp
+++ esphome/components/sofarsolar_inverter/sofarsolar_inverter.cpp
@@ -124,6 +124,24 @@ namespace esphome {
 			if (millis() - zero_export_last_update > 1000 && this->zero_export_) {
 				zero_export_last_update = millis();
 				ESP_LOGD(TAG, "Updating zero export status");
+				// Read the current zero export status
+				G3_dynamic.at(POWER_CONTROL).write_value.uint16_value = 0b00001;
+				G3_dynamic.at(POWER_CONTROL).write_set_value = true;
+
+				G3_dynamic.at(ACTIVE_POWER_IMPORT_LIMIT).write_value.uint16_value = 0;
+				G3_dynamic.at(ACTIVE_POWER_IMPORT_LIMIT).write_set_value = true;
+
+				G3_dynamic.at(REACTIVE_POWER_SETTING).write_value.int16_value = 0;
+				G3_dynamic.at(REACTIVE_POWER_SETTING).write_set_value = true;
+
+				G3_dynamic.at(POWER_FACTOR_SETTING).write_value.int16_value = 0;
+				G3_dynamic.at(POWER_FACTOR_SETTING).write_set_value = true;
+
+				G3_dynamic.at(ACTIVE_POWER_LIMIT_SPEED).write_value.uint16_value = 1;
+				G3_dynamic.at(ACTIVE_POWER_LIMIT_SPEED).write_set_value = true;
+
+				G3_dynamic.at(REACTIVE_POWER_RESPONSE_TIME).write_value.uint16_value = 0;
+				G3_dynamic.at(REACTIVE_POWER_RESPONSE_TIME).write_set_value = true;
 
 				if (!((battery_charge_only_switch_state_ == true && G3_dynamic.at(MINIMUM_BATTERY_POWER).sensor->state == 0) || (battery_charge_only_switch_state_ == false && G3_dynamic.at(MINIMUM_BATTERY_POWER).sensor->state == -5000) || (battery_discharge_only_switch_state_ == true && G3_dynamic.at(MAXIMUM_BATTERY_POWER).sensor->state == 0) || (battery_discharge_only_switch_state_ == false && G3_dynamic.at(MAXIMUM_BATTERY_POWER).sensor->state == 5000) || (model_parameters.at(this->model_id_).max_output_power_w == G3_dynamic.at(DESIRED_GRID_POWER).sensor->state))) {
 					G3_dynamic.at(DESIRED_GRID_POWER).write_value.int32_value = model_parameters.at(this->model_id_).max_output_power_w;
