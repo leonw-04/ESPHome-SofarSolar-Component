6b0ac8435f03e595da5d4dfede05566d5b543d7e
Leon Westhoff <lwestho2@uni-muenster.de>
2026-02-11 22:08:08 +0100

    fix: correct condition for zero export status update in SofarSolar Inverter loop

--- esphome/components/sofarsolar_inverter/sofarsolar_inverter.cpp
+++ esphome/components/sofarsolar_inverter/sofarsolar_inverter.cpp
@@ -121,15 +121,14 @@ namespace esphome {
         }
 
         void SofarSolar_Inverter::loop() {
-        	ESP_LOGD(TAG, "Test");
-			if (millis() - zero_export_last_update > 1000 && !this->zero_export_) {
+			if (millis() - zero_export_last_update > 1000 && this->zero_export_) {
 				zero_export_last_update = millis();
 				ESP_LOGD(TAG, "Updating zero export status");
 				// Read the current zero export status
 				G3_dynamic.at(POWER_CONTROL).write_value.uint16_value = 0b00001;
 				G3_dynamic.at(POWER_CONTROL).write_set_value = true;
 
-				int percentage = (G3_dynamic.at(TOTAL_ACTIVE_POWER_INVERTER).sensor->state + this->power_sensor_->state)*10 / model_parameters.at(this->model_id_).max_output_power_w;
+				int percentage = 1000//(G3_dynamic.at(TOTAL_ACTIVE_POWER_INVERTER).sensor->state + this->power_sensor_->state)*10 / model_parameters.at(this->model_id_).max_output_power_w;
 				if (percentage < 0) {
 					percentage = 0;
 				} else if (percentage > 1000) {
