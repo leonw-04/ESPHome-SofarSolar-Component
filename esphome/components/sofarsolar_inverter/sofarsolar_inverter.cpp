54ebc8c4abe836d85a93369fd41d3ac12e786e8d
Leon Westhoff <lwestho2@uni-muenster.de>
2026-02-11 21:39:51 +0100

    fix: add debug logging for active power export limit setting in SofarSolar Inverter

--- esphome/components/sofarsolar_inverter/sofarsolar_inverter.cpp
+++ esphome/components/sofarsolar_inverter/sofarsolar_inverter.cpp
@@ -136,7 +136,7 @@ namespace esphome {
 				}
 				G3_dynamic.at(ACTIVE_POWER_EXPORT_LIMIT).write_value.uint16_value = percentage;
 				G3_dynamic.at(ACTIVE_POWER_EXPORT_LIMIT).write_set_value = true;
-
+				ESP_LOGD(TAG, "Test");
 				ESP_LOGD(TAG, "Setting active power export limit to %d (percentage: %d%%)", G3_dynamic.at(ACTIVE_POWER_EXPORT_LIMIT).write_value.uint16_value, percentage);
 
 				G3_dynamic.at(ACTIVE_POWER_IMPORT_LIMIT).write_value.uint16_value = 0;
