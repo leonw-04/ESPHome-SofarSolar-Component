be6182f0cf0f14571e9808332c59690279c0f628
Leon Westhoff <leon.westhoff@pannek.eu>
2026-03-14 22:38:40 +0100

    fix: initialize time_begin_modbus_operation when enforcing default value for register key

--- esphome/components/sofarsolar_inverter/sofarsolar_inverter.cpp
+++ esphome/components/sofarsolar_inverter/sofarsolar_inverter.cpp
@@ -866,6 +866,7 @@ namespace esphome
             if (G3_dynamic.at(register_key).enforce_default_value && G3_dynamic.at(register_key).default_value_set) {
                 ESP_LOGD(TAG, "Enforce default value is set for register key: %d", register_key);
 				if(G3_dynamic.at(register_key).default_value.uint16_value != value) {
+					time_begin_modbus_operation = millis();
 					current_writing = true;
 					this->write_register(register_key);
 					G3_dynamic.at(register_key).last_update = 0; // Reset the last update time to force an update in the next loop
